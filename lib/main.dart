d81b6c7083f8c23f2afd6da7caee525e8779eccd
JaneGH <jkrytsyna@gmail.com>
2025-03-24 14:21:15 -0700

    added auth to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_app/screens/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_chat_app/screens/auth.dart';
+import 'package:flutter_chat_app/screens/chat.dart';
+import 'package:flutter_chat_app/screens/splash.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async {
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
-      name: 'flutter_chat_app',
-      options: DefaultFirebaseOptions.currentPlatform,
+    name: 'flutter_chat_app',
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const App());
 }
@@ -20,14 +24,24 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'FlutterChat',
-        theme: ThemeData().copyWith(
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)
-          ),
-        ),
-        home: AuthScreen(),
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+         colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
 
+            if (snapshot.hasData) {
+              return const ChatScreen();
+            }
+
+            return const AuthScreen();
+          }),
     );
   }
 }
